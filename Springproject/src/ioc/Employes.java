b1986413a07b4b204ba7f7007938ef0a09992ba5
Albertpilaestrada <125912097+Albertpilaestrada@users.noreply.github.com>
2023-04-05 14:48:27 +0200

    Add files via upload

---
+++ Springproject/src/ioc/Employes.java
@@ -0,0 +1,9 @@
+package ioc;
+
+public interface Employes {
+	
+	public String gettask();
+	
+	public String getinforme();
+
+}
---
+++ Springproject/src/ioc/Reports.java
@@ -0,0 +1,7 @@
+package ioc;
+
+public interface Reports {
+	
+	public String getinforme();
+
+}
